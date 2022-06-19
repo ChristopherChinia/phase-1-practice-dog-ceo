d284ecf9ceeb27969312aec7dc47e3684ed1d6f0
christopherchinia <chinia214@gmail.com>
2022-06-19 14:32:28 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -1,1 +1,28 @@
 console.log('%c HI', 'color: firebrick')
+
+fetch = "https://dog.ceo/api/breeds/image/random/4"
+
+
+fetch(imgUrl)
+.then(res =>res.json())
+.then((images) => {console.log(images)
+   for (const item of images.message) {
+       const pictures = document.querySelector("#dog-image-container");
+       pictures.innerHTML += `<img src="${item}" alt=""> `
+   }
+})
+
+const breedUrl = 'https://dog.ceo/api/breeds/list/all'
+fetch(breedUrl)
+.then(res=>res.json())
+.then(breeds =>{console.log(breeds)
+    for(const item in breeds.message){
+        let list=document.querySelector("#dog-breeds")
+        const li=document.createElement("li")
+        li.innerHTML=item
+        list.appendChild(li);
+        li.addEventListener('click', ()=>{
+            list.style.color="red"
+        })
+    }
+})
